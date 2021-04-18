3d076d22b5cccf884e812980c992dfe7fc05ff40
hallamargret <hallamargret99@gmail.com>
2021-04-18 20:51:23 +0000

    the assignment as i handed it in

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+
+var templateBoard = document.getElementById("templateboard");
+var templateTask = document.getElementById("templateTask");
+
+
+
 var input = document.getElementById("inputbox");
     input.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
             event.preventDefault();
-            document.getElementById("myBtnCreate").click();
+            document.getElementById("boardCreateBtn").click();
+            console.log("enter pressed")
+        }
+    });
+
+var input = document.getElementById("taskInputBox");
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("CreateTaskBtn").click();
             console.log("enter pressed")
   }
 });
+
 
 
 
@@ -27,20 +43,38 @@ function drawBoard () {
         //making the delete button
         var delButton = document.createElement("button");
         delButton.textContent = "X";
-        delButton.id = "delButt";
+        delButton.id = "delButt"+ i;
+        delButton.className="delButt";
         myBoard.appendChild(delButton);
 
         var header = document.createElement("h2");
-        header.id="boardtitle"
+        header.id="boardtitle"+ i;
         header.textContent = boards[i].name;
 
+
+        var taskInput = document.createElement("input");
+        taskInput.id = "taskInputBox"+ i;
+        taskInput.placeholder = "Create new task";
+        
+
         myBoard.appendChild(header);
+        myBoard.appendChild(taskInput);
         container.appendChild(myBoard);
     }
 };
 
 drawBoard();
 
+
+function CreateNewBoard(){
+    var boardNew = templateBoard.cloneNode(true);
+    boardNew.id = document.getElementsByClassName('boardClass').length;
+    //boardNew.children[2].innerHTML = document.getElementById
+    var inputField = document.getElementById('inputbox');
+    var inputValue = inputField.value;
+    boardNew.name = inputValue;
+
+};
 
 
 
@@ -61,19 +95,41 @@ function createBoard () {
 
     //making the delete button
     var delButton = document.createElement("button");
-    delButton.id = "delButt";
+    delButton.id = "delButt"+ divCounter;
     delButton.textContent = "X";
     myDiv.appendChild(delButton);
 
-
+    //making the board title
     var header = document.createElement("h2");
-    header.id="boardtitle"
+    header.id="boardtitle"+ divCounter;
     header.textContent = myDiv.name;
     myDiv.appendChild(header);
+
+
+    //making the create new task input
+
+    var taskInput = document.createElement("input");
+    taskInput.id = "taskInputBox" + divCounter;
+    taskInput.placeholder = "Create new task"
+    myDiv.appendChild(taskInput);
 
     console.log(myDiv.id.substring(9));
 
     //Put the div into the board container
     var container = document.getElementById('boardcontainer');
     container.appendChild(myDiv);
+};
+
+
+function createTask(){
+    var newCreatedTask = templateTask.cloneNode(true);
+    //var boardOfTask= 
+};
+
+
+function delBoard(btn){
+    var board = btn.parentElement;
+    console.log(board)
+    //if board has no tasks then we may delete
+    board.remove()
 };
